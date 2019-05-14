c892097884207169a2ef0fb15b21280d66df4b0d
Chris Xiao <csx773@gmail.com>
2019-05-14 01:37:15 +0000

    created project files

---
+++ main.rb
@@ -0,0 +1,15 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
+require 'pry'
+require './game'
+require './player'
+require './question'
+
+## Your test code can go here
+puts "=== START OF MAIN.RB ===="
+
+
+
+
+puts "=== END OF MAIN.RB ==="

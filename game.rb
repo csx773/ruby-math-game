cd260d6df5f6e7d43501c2a3ebcb738ff62fbf3d
Chris Xiao <csx773@gmail.com>
2019-05-14 02:31:00 +0000

    game class: get_player and remove_player methods work

--- game.rb
+++ game.rb
@@ -2,7 +2,32 @@ require './player'
 require './question'
 
 class Game
+    # attr_reader :question, :answer
+    # attr_writer :name
+    attr_accessor :players
 
+    def initialize 
+        # will contain all players for game (min 2)
+        @players = []
+        
+    end
 
+    def get_player newPlayer
+    #grab player name and push into players[]
+        @players.push(newPlayer.name)
+    end
+
+    def start_game
+    # init game settings and loop
+    end
+
+    def turn 
+        #logic for actions every turn
+    end
+
+    def remove_player player_name
+        #removes the player with life = 0
+        @players.delete_if { |name| name == player_name } 
+    end
 
 end
--- main.rb
+++ main.rb
@@ -9,20 +9,34 @@ require './question'
 ## Your test code can go here
 puts "=== START OF MAIN.RB ===="
 
-question1 = Question.new
-puts question1.question
-puts question1.answer
+# question1 = Question.new
+# puts question1.question
+# puts question1.answer
 
-input = gets.chomp
-puts question1.guess_answer?(input)
+# input = gets.chomp
+# puts question1.guess_answer?(input)
 
-Ted = Player.new('Ted')
-puts Ted.name
-puts Ted.life
-Ted.remove_life
-Ted.remove_life
+# Ted = Player.new('Ted')
+# puts Ted.name
+# puts Ted.life
+# Ted.remove_life
+# Ted.remove_life
 
-puts "current life of player is " + Ted.life.to_s
+# puts "current life of player is " + Ted.life.to_s
+
+player1 = Player.new("Sam")
+player2 = Player.new("Ben")
+
+mathGame = Game.new
+puts mathGame.players
+mathGame.get_player(player1)
+mathGame.get_player(player2)
+puts "Current players in game are " + mathGame.players.to_s
+
+mathGame.remove_player("Ben")
+puts "Current players in game are " + mathGame.players.to_s
+
+
 
 
 puts "=== END OF MAIN.RB ==="
